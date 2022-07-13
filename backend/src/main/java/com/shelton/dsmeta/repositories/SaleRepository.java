654a1dd2b85b07c3aa50b20672621605dd7f2994
Shelton Francisco <sheltonfjs@gmail.com>
2022-07-13 17:41:51 +1400

    date select

--- backend/src/main/java/com/shelton/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/shelton/dsmeta/repositories/SaleRepository.java
@@ -1,6 +1,12 @@
 package com.shelton.dsmeta.repositories;
 
+
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.shelton.dsmeta.entities.Sale;
 
@@ -9,5 +15,8 @@ import com.shelton.dsmeta.entities.Sale;
 public interface SaleRepository extends JpaRepository<Sale, Long> { //<Tipo do objecto(classe), Tipo da chave Primaria>
 	
 	// estendendo a interface JpaRepository, temos acesso a todos os metodos necessarios para interagir com o banco (pesquisa, isercao......etc...)
-		
+	
+
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
