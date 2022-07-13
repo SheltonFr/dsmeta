d9a62a87dfa653a1cf9f4c251d4beb74450933fd
Shelton Francisco <sheltonfjs@gmail.com>
2022-07-13 16:11:53 +1400

    dataBase h2

---
+++ backend/src/main/java/com/shelton/dsmeta/entities/Sale.java
@@ -0,0 +1,77 @@
+package com.shelton.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale { // entidade de Venda
+
+	@Id  // indica que o elemento logo abaixo deve ser unico no banco de dados (primary Key)
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // gerar id de forma aautoma
+	private Long id;
+	private String sellerName;
+	private Integer visited; // numero de visitas
+	private Integer deals; // vendas feitas
+	private Double amount; // quantia total
+	private LocalDate date;
+
+	public Sale() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	
+}
